f2c1af1a5e7fc137cf8b7e63448fe10cb2437aaf
mdfaizan973 <faizan.md9735@gmail.com>
2025-03-24 03:02:58 +0530

    aa

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const UserModel = require("../models/UserModel");
 const UserRouter = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 
 /**
