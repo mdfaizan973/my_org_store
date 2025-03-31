a9d333e2c2ca6c363e471f056bb296e5ac195761
mdfaizan973 <faizan.md9735@gmail.com>
2025-03-31 15:24:48 +0530

    added api for saler data

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -38,6 +38,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
+    description: {
+      type: String,
+      required: false,
+    },
   },
   { timestamps: true }
 );
--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -4,6 +4,8 @@ const UserRouter = express.Router();
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 
+const updateMissingFields = require("../utils/updateMissingFields");
+updateMissingFields(UserModel);
 /**
  * @route   POST /registers
  * @desc    Register a new user
